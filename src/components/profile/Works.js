b09f61257290fd754de4460e3840368702c11f77
AshishBhardwaj1 <fireash70@gmail.com>
2023-10-30 17:18:16 +0530

    first commit

---
+++ src/components/profile/Works.js
@@ -0,0 +1,102 @@
+import React from 'react'
+import "./works.css"
+
+const artist =[
+  {
+id:1,
+name:"Thomas Edward",
+profile:"./artist-images/pexels-anastasia-shuraeva-4406721.png",
+bgimg :  "./artist-images/pexels-ekaterina-12203460.png",
+tag:"@thewildwithyou"     
+  
+},
+
+{
+id:2,
+name:"Chris Doe",
+profile:"./artist-images/pexels-andrea-piacquadio-3771118.png ",
+bgimg :  "./artist-images/pexels-genaro-servín-763210.png",
+tag:"@thewildwithyou"     
+},
+
+{
+id:3,
+name:"Emilie Jones",
+profile:"./artist-images/pexels-antoni-shkraba-4442005.png ",
+bgimg :   "./artist-images/pexels-pixabay-164455.png",
+tag:"@thewildwithyou"     
+},
+
+{
+id:4,
+name:"Jessica Williams",
+profile:" ./artist-images/pexels-antoni-shkraba-4442102.png",
+bgimg :  "./artist-images/pexels-steve-johnson-1269968.png",
+tag:"@thewildwithyou"     
+},
+{
+id:5,
+name:"Thomas Edward",
+profile:"./artist-images/pexels-anastasia-shuraeva-4406721.png",
+bgimg :  "./artist-images/pexels-fiona-art-5022849.png",
+tag:"@thewildwithyou"     
+},
+]
+
+const Works = () => {
+  return (
+    <div className='work-profile'>
+
+<div className="seller-button">
+
+ <button>become a seller</button>
+
+</div>
+
+
+<div className="artists" >
+<span className='category'>artist</span>
+<span className='category'>photographers</span>
+
+
+{
+  artist.map((artists)=>(
+
+<div className="card" key={artists.id}  >
+  <div className="artist-img">
+    <img src= {artists.bgimg} alt="image" />
+ </div>
+ <div className="artist-info">
+  <div className="img">
+    <img src={artists.profile} alt="" />
+  </div>
+ 
+  <div>
+
+  <div className="name"> {artists.name} </div>
+  <div className="profession"> {artists.tag}</div>
+  </div>
+ </div>
+</div>
+
+
+
+
+
+  ))
+}
+
+
+
+
+
+</div>
+
+
+</div>
+
+    
+  )
+}
+
+export default Works
---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,80 @@
+import React from 'react'
+import home from "./icons/home.png"
+import message from "./icons/message.png"
+import favorite from "./icons/favorite.png"
+import heart from "./icons/heart.png"
+import notification from "./icons/notification.png"
+import wallet from "./icons/wallet.png"
+import logout from "./icons/logout.png"
+import setting from "./icons/setting.png"
+import profile from "./icons/profile.png"
+
+import "./sidebar.css"
+const Sidebar = () => {
+  return (
+    <div className='sidebar'> 
+ 
+   <div className="logo">
+    <span> LOGO </span>  </div>
+
+        <div className="sidebar-items">
+        
+             <div className="sidebar-item" > 
+             <img src= {home} alt="img" />
+             
+             Home</div>
+
+             <div className="sidebar-item">
+                
+           <img src={notification} alt="" />
+                 Notifications </div>
+             <div className="sidebar-item"> 
+             
+             <img src={heart} alt="" />
+             Shop</div>
+
+             <div className="sidebar-item">
+                <img src={message} alt="" />
+                 Conversation</div>
+             
+             <div className="sidebar-item">
+                <img src={wallet} alt="" />
+
+                Wallet </div>
+             <div className="sidebar-item">
+                
+                <img src={favorite} alt="" />
+                 Subscription </div>
+             <div className="sidebar-item"> 
+            <img src= {profile} alt="" />
+             My Profile</div>
+
+             <div className="sidebar-item"> 
+             
+             <img src={setting} alt="" />
+             Settings </div>
+            
+
+
+    <div className="login" style={{
+        color:"#88c2bb",
+        position:"absolute",
+        bottom:"20px"
+    }}> 
+    <img src={logout} alt="" />
+Log out
+    </div>
+        </div>
+ 
+
+
+              <div className="footer">
+                <span> 2022©logo name</span>
+                 <span>Developed by ivan Infotech </span>
+              </div>
+
+    </div>
+  )
+}
+
+export default Sidebar
