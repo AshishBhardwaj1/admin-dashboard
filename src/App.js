a4de12db111131e400636668e754ccc4a045fe32
sabyasachirakshit <sabyasachirakshitacs19@gmail.com>
2023-10-30 18:45:14 +0530

    formatted

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-
-
-import Profile from './components/timeline/Profile';
-import Sidebar from './components/sidebar/Sidebar';
-import Works from './components/profile/Works';
+import Profile from "./components/timeline/Profile";
+import Sidebar from "./components/sidebar/Sidebar";
+import Works from "./components/profile/Works";
 
 function App() {
   return (
-    <div className="App" style={{backgroundColor:"#f5f5f5",display:"flex", justifyContent:"center", flexWrap:"wrap" }}>
-    <Sidebar/>
-    <Profile/>
-    <Works/>
+    <div
+      className="App"
+      style={{
+        backgroundColor: "#f5f5f5",
+        display: "flex",
+        justifyContent: "center",
+        flexWrap: "wrap",
+      }}
+    >
+      <Sidebar />
+      <Profile />
+      <Works />
     </div>
   );
 }
